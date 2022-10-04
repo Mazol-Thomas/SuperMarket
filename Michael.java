f990bdb398334c3bbdc1830e65c28db2dd926197
Mazol Thomas Biem <biemtop718@gmail.com>
2022-10-04 20:12:14 +0300

    push michael
    my personal branch

---
+++ Michael.java
@@ -0,0 +1,12 @@
+public class Michael
+{
+    public static void main(String[]args)
+    {
+        int a = 90;
+        int b = 100;
+
+        int c = a + b;
+
+        System.out.println("The Final Addition Answer is: " + c);
+    }
+}
