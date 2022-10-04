d490f717c4be2f8d6b5b5617a04de1ebb7c570a7
Mazol Thomas Biem <biemtop718@gmail.com>
2022-10-04 20:02:36 +0300

    updating supermarket
    launching new products

---
+++ James.java
@@ -0,0 +1,12 @@
+public class James
+{
+    public static void main(String[]args)
+    {
+        int a = 1000;
+        int b = 2000;
+
+        int c = a + b;
+
+        System.out.println("The Final Addition Answer is: " + c);
+    }
+}
