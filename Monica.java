2cfa2797e6217a87db241cbd44fde89d86e6f2cd
Mazol Thomas Biem <biemtop718@gmail.com>
2022-10-05 12:04:52 +0300

    updating UKmall
    A new thing added to the origin and UK mall

---
+++ Monica.java
@@ -0,0 +1,13 @@
+public class Monica
+{
+    public static void main(String[]args)
+    {
+        int a = 12;
+        int b = 90;
+        int c = 45;
+        int d = a + b;
+        int e = d * c;
+
+        System.out.println("The Final Addition Answer is: " + e);
+    }
+}
